dbe273ab8a4f24d66dfa650339bce846a12e27f0
R-Shirakawa <133877078+R-Shirakawa@users.noreply.github.com>
2024-06-25 01:49:52 +0900

    redirectToPetDetail.js を更新

--- src/main/resources/static/js/redirectToPetDetail.js
+++ src/main/resources/static/js/redirectToPetDetail.js
@@ -1,5 +1,3 @@
 function redirectToPetDetail(catId) {
-//    var catId = $(".cat-id").last().data("cat-id");
-    // 写真押下時にpetDetailへpetIdを渡すフォームをサブミットする
     document.getElementById('petDetailForm').submit();
 }
