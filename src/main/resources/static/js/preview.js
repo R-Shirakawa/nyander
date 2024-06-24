12eadd88f116620618bf58948bfa484a34c09fe7
R-Shirakawa <133877078+R-Shirakawa@users.noreply.github.com>
2024-06-25 01:52:31 +0900

    preview.js を更新

--- src/main/resources/static/js/preview.js
+++ src/main/resources/static/js/preview.js
@@ -1,40 +1,31 @@
 function preview(obj, previewId) {
-    // Clear existing previews
+    
     document.querySelector(`#${previewId}`).innerHTML = '';
 
-    // 選択した全てのファイルに対してループ処理をします。
     for (i = 0; i < obj.files.length; i++) {
-        // FileReaderオブジェクトを作成します。これによりブラウザ上でファイルを読み込むことが可能になります。
         let fileReader = new FileReader();
 
-        // onloadイベントハンドラを設定します。ファイルが正常に読み込まれたときに呼び出されます。
+        // ファイルが正常に読み込まれたときに呼び出す
         fileReader.onload = (e) => {
             if (obj.accept.includes('image')) {
-                // 読み込んだ画像ファイルをData URLとしてimg要素に設定します。
-                // これにより、選択した画像がブラウザ上でプレビュー表示されます。
+                // 読み込んだ画像ファイルをData URLとしてimg要素に設定
                 let imgElement = document.createElement('img');
                 imgElement.src = e.target.result;
 
-                // Add a class to the img element
                 imgElement.classList.add('img-preview');
 
-                // Append the img element to the specified preview div
                 document.querySelector(`#${previewId}`).appendChild(imgElement);
             } else if (obj.accept.includes('video')) {
-                // 読み込んだ動画ファイルをData URLとしてvideo要素に設定します。
+                // 読み込んだ動画ファイルをData URLとしてvideo要素に設定
                 let videoElement = document.createElement('video');
                 videoElement.src = e.target.result;
-                videoElement.controls = true; // Add controls to the video element
-
-                // Add a class to the video element
+                videoElement.controls = true;
                 videoElement.classList.add('video-preview');
-
-                // Append the video element to the specified preview div
                 document.querySelector(`#${previewId}`).appendChild(videoElement);
             }
         };
 
-        // ファイルをData URLとして読み込みます。
+        // ファイルをData URLとして読み込み
         fileReader.readAsDataURL(obj.files[i]);
     }
 }
